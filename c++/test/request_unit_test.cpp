2311c8594d04a602a943fa9ccf9b59ce3db9c43d
Geoffrey Atkinson <geoffrey.j.atkinson@gmail.com>
2019-03-19 18:32:12 -0400

    Get inital extension building

---
+++ c++/test/request_unit_test.cpp
@@ -0,0 +1,13 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+class RequestUnitTest : public ::testing::Test
+{
+protected:
+    RequestUnitTest() {}
+};
+
+TEST_F(RequestUnitTest, Instantiate)
+{
+
+}
---
+++ c++/test/response_unit_test.cpp
@@ -0,0 +1,13 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+class ResponseUnitTest : public ::testing::Test
+{
+protected:
+    ResponseUnitTest() {}
+};
+
+TEST_F(ResponseUnitTest, Instantiate)
+{
+
+}
