3b9d44e231ca085a4e87465e842082d55cde896a
Geoffrey Atkinson <geoffrey.j.atkinson@gmail.com>
2019-03-19 20:17:15 -0400

    Integrate CDash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,5 +1,5 @@
-set(CTEST_PROJECT_NAME       "challenge")
-set(CTEST_NIGHTLY_START_TIME "02:00:00 CEST")
+set(CTEST_PROJECT_NAME       "Challenge")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 set(CTEST_DROP_METHOD        "http")
 set(CTEST_DROP_SITE          "my.cdash.org")
 set(CTEST_DROP_LOCATION      "/submit.php?project=Challenge")
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # challenge
 Yet another code challenge
 
+CDash is located at: 
+https://my.cdash.org/index.php?project=Challenge
+
+
+Build status:
 [![Build Status](https://travis-ci.org/gatkinso/challenge.svg?branch=master)](https://travis-ci.org/gatkinso/challenge)
 
