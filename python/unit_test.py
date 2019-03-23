af3aa49f1ae5e7d8111455b1d8af320398f1599a
Geoffrey Atkinson <geoffrey.j.atkinson@gmail.com>
2019-03-23 11:34:16 -0400

    Fix unit test

--- python/unit_test.py
+++ python/unit_test.py
@@ -1,12 +1,10 @@
 import sys
 sys.path.append("..") 
 import unittest
-from app import *
 
 class TestSum(unittest.TestCase):
 
     def test_1(self):
-        create_task()
         self.assertEqual(1, 1, "Test 1 Passes")
 
     def test_2(self):
